62a07d4daf45ee553662a42ab081825e883c9e5d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-04-03 13:49:25 -0500

    Scripted auto-commit on change (2018-04-03 13:49:25) by gitwatch.sh

---
+++ blink/Ingenious Kasi-Albar1.ino
@@ -0,0 +1,14 @@
+// Code that is run once before the loop kicks in
+// This is a "function" - mini program
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
